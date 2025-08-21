1e518a44c63c723c46a005fcffa57d40ea5e4035
Hodaya32 <115700920+Hodaya32@users.noreply.github.com>
2025-08-21 21:27:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const topics = [
+    "האם עומר בעצם פיטר פן",
+    "הכל חוץ מנופיקי",
+    "שינוי אקלים ",
+    "חתולים",
+    "עוד חתולים",
+    "כלום פשוט סתמו",
+    "בואו משחק הציורים",
+    "בואו נתאבד קבוצתית ",
+    "האם אני צריכה תחביבים אחרים",
+   "האם יודו באמת קיים",
+   "למה תומר מכיר כל שיר בעולם"
+   
+];
+
+function generateTopic() {
+    const randomIndex = Math.floor(Math.random() * topics.length);
+    document.getElementById("topic").innerText = topics[randomIndex];
+}
